8bbd19531a5743ea942018072bb3de14422f1776
Zoeyzyy <1162651439@qq.com>
2025-03-21 12:23:45 +0000

    Fix: use refer count to show unsafety

--- code/l5-1-unsafe-sharedptr.cpp
+++ code/l5-1-unsafe-sharedptr.cpp
@@ -4,28 +4,30 @@
 #include <vector>
 
 void incrementSharedPtr(std::shared_ptr<int>& ptr) {
-    for (int i = 0; i < 1000; ++i) {
-        std::shared_ptr<int> temp = ptr; 
-        ++(*temp);
-        ptr = temp; 
+    for (int i = 0; i < 100000; ++i) {
+        std::shared_ptr<int> localPtr = ptr; // 复制 shared_ptr
+        // 模拟一些工作
+        std::this_thread::sleep_for(std::chrono::microseconds(1));
     }
 }
 
 int main() {
-    std::shared_ptr<int> ptr = std::make_shared<int>(0);
+    // 创建一个 shared_ptr
+    std::shared_ptr<int> ptr = std::make_shared<int>(42);
 
+    // 创建多个线程来增加 shared_ptr 的引用计数
     std::vector<std::thread> threads;
-    for (int i = 0; i < 1000; ++i) {
+    for (int i = 0; i < 10; ++i) {
         threads.emplace_back(incrementSharedPtr, std::ref(ptr));
     }
 
+    // 等待所有线程完成
     for (auto& t : threads) {
         t.join();
     }
 
-    std::cout << "Value should be " << 1000000 << std::endl;
-    std::cout << "Value: " << *ptr << std::endl;
-
+    // 输出引用计数
+    std::cout << "引用计数: " << ptr.use_count() << std::endl;
     return 0;
 }
 
